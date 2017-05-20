a631796ae7ccadd9e1bf33909d4feb9fbbdbf280
Jeff Moore <jmas1_2000@hotmail.com>
2017-05-20 22:01:15 +0000

    User Story 5 Seeding Data: Completed

--- Gemfile
+++ Gemfile
@@ -32,3 +32,5 @@ end
  gem 'devise' 
 
 gem 'pundit'
+
+gem 'faker'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+10.times do
+  User.create!(
+    email: Faker::Internet.unique.email,
+    password: Faker::Internet.password,
+    name: Faker::Name.name
+    )
+end
+
+users = User.all
+
+40.times do
+  Item.create!(
+    user: users.sample,
+    body: Faker::Lorem.sentence
+    )
+end
+
+
+
+puts "#{User.count} users created"
+puts "#{Item.count} items created"
